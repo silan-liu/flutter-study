75959c38c5f30cdfdf84082ed19581ea3ce4b83b
刘思兰 <liusilan@douyu.tv>
2020-08-04 21:45:27 +0800

    init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/liusilan/Downloads/software/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/liusilan/Documents/workspace/my/flutter/hello"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/liusilan/Downloads/software/flutter/bin/cache/artifacts/engine/ios"
